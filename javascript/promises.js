e9e5cbeb40970bced51bf2f5cfd5bd8e28ddcb1f
thorjarhun <thorjarhun@gmail.com>
2015-09-16 15:34:03 -0500

    Minor updates

--- javascript/promises.js
+++ javascript/promises.js
@@ -15,3 +15,19 @@ delay(1000).then(function() {
     console.log(2);
   });
 });
+
+
+// Example 2
+Promise.resolve().then(function(data) {
+  console.log(1);
+  return new Promise(function(done) {
+    setTimeout(done, 1000, 2);
+  });
+}).then(function(data) {
+  console.log(data);
+  return new Promise(function(done) {
+    setTimeout(done, 1000, 3);
+  });
+}).then(function(data) {
+  console.log(data);
+});
